2540e266af79ebde4fc56499a10c258f93a31e27
TomasCalcopietro <tomascalcopietro0@gmail.com>
2022-10-12 19:15:05 -0300

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import useQuiosco from "../hooks/useQuiosco"
+import Categoria from "./Categoria"
+import { useRouter } from "next/router"
+
+const Sidebar = () => {
+
+  const { categorias } = useQuiosco()
+
+  const router = useRouter()
+
+  return (
+    <>
+        <button>
+          <Image 
+              width={300} 
+              height={100} 
+              src="/assets/img/logo.svg" 
+              alt="imagen-logotipo"
+              onClick={() => {
+                router.push("/")
+            }}
+
+          />
+        </button>
+
+        <nav className="mt-10">
+          {categorias.map(categoria => (
+              <Categoria
+                key={categoria.id}
+                categoria={categoria}
+              />
+          ))}
+        </nav>
+    </>
+  )
+}
+
+export default Sidebar
